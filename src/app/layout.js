57a8236fc011ffb1cbbefed8a531a6f8e6938b41
Bae Jinwoo <gravity0225@naver.com>
2025-08-30 23:28:18 +0900

    tag error fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+// src/app/layout.js
 import '../../styles/globals.css';
 import Footer from './(components)/Footer';
 import Navbar from './(components)/navbar';
@@ -5,62 +6,46 @@ import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Toaster } from 'react-hot-toast';
 import Script from 'next/script';
-import Head from 'next/head';
 import { ProjectProvider } from './context/projectContext';
 
 export const metadata = {
   title: 'DO IT',
   description: '연세대학교 미래캠퍼스 IT협업동아리',
-  icons: {
-    icon: '/favicon.ico',
-  },
+  icons: { icon: '/favicon.ico' },
   openGraph: {
     title: 'DO IT',
     description: '연세대학교 미래캠퍼스 IT협업동아리',
     url: 'https://do-it.kr',
     type: 'website',
-    images: [
-      {
-        url: '/DO+IT_Logo_Black+Blue.png',
-        width: 800,
-        height: 600,
-        alt: '연세대학교 미래캠퍼스 IT협업동아리',
-      },
-    ],
+    images: [{ url: '/DO+IT_Logo_Black+Blue.png', width: 800, height: 600, alt: '연세대학교 미래캠퍼스 IT협업동아리' }],
   },
   metadataBase: new URL('https://do-it.kr'),
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html>
-      <ProjectProvider>
-        <Head>
-          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-M22XJQRRYQ" />
-          <Script id="gtag-init" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-M22XJQRRYQ');
-            `}
-          </Script>
-          <title>DO IT</title>
-          <meta
-            name="description"
-            content="연세대학교 미래캠퍼스 IT협업동아리"
-          />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <body>
+    <html lang="ko">
+      <body>
+        {/* GA4 gtag */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-M22XJQRRYQ" />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-M22XJQRRYQ');
+          `}
+        </Script>
+
+        <ProjectProvider>
           <Toaster position="top-center" reverseOrder={false} />
           <Navbar />
           {children}
           <Analytics />
           <SpeedInsights />
           <Footer />
-        </body>
-      </ProjectProvider>
+        </ProjectProvider>
+      </body>
     </html>
   );
 }
